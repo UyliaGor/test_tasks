f907b81f387730d308745425a46701bf61f836a7
Julia Gorelikova <jkireenko137@gmail.com>
2021-09-17 12:49:09 +0300

    добавлено решение задачи 1

---
+++ task_1.py
@@ -0,0 +1,10 @@
+import random
+
+numbers = []
+for _ in range(10):
+    num = random.random()
+    numbers.append(num)
+
+print('Максимальное значение =', max(numbers))
+print('Минимальное значение =', min(numbers))
+print('Среднее значение', sum(numbers)/len(numbers))
